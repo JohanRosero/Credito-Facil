2009a2cb94b4669ea056569c561296cb64c4d05d
JOHAN CRISTHIAN ROSERO ROSERO <130606281+JohanRosero@users.noreply.github.com>
2025-01-04 04:22:24 -0300

    implement user registration with email verification and update loading state in register component

--- Frontend-CF/src/app/services/authenticate.service.ts
+++ Frontend-CF/src/app/services/authenticate.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  sendEmailVerification
+} from 'firebase/auth';
+import { environment } from '../environments/enviroment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
 
-  constructor() { }
+  private app = initializeApp(environment.firebase);
+  private auth = getAuth(this.app);
+
+  constructor(private router: Router) {
+
+  }
+
+  async register(email: string, password: string) {
+    try {
+      return await createUserWithEmailAndPassword(this.auth, email, password)
+        .then((result) => {
+          sendEmailVerification(result.user)
+        });
+    } catch (error) {
+      return error;
+    }
+  }
+
+
 }
--- Frontend-CF/src/app/views/pages/register/register.component.ts
+++ Frontend-CF/src/app/views/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import { AuthenticateService } from '../../../services/authenticate.service';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +15,31 @@ export class RegisterComponent {
   public repetpassword: string = "";
   public message: string = "";
   public type: string = "";
-  public loadingregister: boolean = true;
+  public loadingregister: boolean = false;
 
-  constructor() { }
+  constructor(private authenticateService: AuthenticateService) { }
 
+  public async register() {
+    if (this.email === "" || this.password === "" || this.repetpassword === "") {
+      this.message = "Please fill in all fields";
+      this.type = "danger";
+    } else if (this.password !== this.repetpassword) {
+      this.message = "Passwords do not match";
+      this.type = "danger";
+    } else {
+      this.loadingregister = true;
+      this.authenticateService.register(this.email, this.password)
+        .then(() => {
+          this.loadingregister = false;
+          this.message = "User registered successfully, please verify your email to confirm your account";
+          this.type = "success";
+        }).catch((error) => {
+          this.loadingregister = false;
+          this.message = error.message;
+          this.type = "danger";
+        });
+    }
+  }
 }
+
+
